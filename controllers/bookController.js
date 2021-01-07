f972346c9fb977a1a9a2a6bd49dde87af34446f4
Peter Dinis <peterdinis0@pobox.sk>
2021-01-07 20:13:35 +0100

    Update books and start implement tests

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -5,7 +5,19 @@ exports.getBooks = (req, res) => {
 }
 
 exports.postBooks = (req, res) => {
-
+    const book = new Book({
+        name: req.body.name,
+        year: req.body.year,
+        info: req.body.info,
+        numPages: req.body.numPages,
+        publisher: req.body.publisher,
+        publishedPhone: req.body.publishedPhone,
+        publishedDate: req.body.publishedDate
+    })
+    book.save();
+    return res.json({
+        message: 'Book was edit to db', book
+    })
 }
 
 exports.getOneBook = (req, res) => {
